f9cf4c1fef5ec1f5ea298360191dd87e92871b1f
João Gabriel <main.joaogabrielsg@gmail.com>
2021-10-25 15:27:10 -0300

    feat: add cart functions
    - Add render cart function
    - Add handle add item to cart function
    - Add update cart total price function
    - Add functions to make cart item elements and render on screen
    - Add delete cart item by id function
    - Add format currency to BRL function

--- src/js/index.js
+++ src/js/index.js
@@ -12,6 +12,10 @@
 
     const $buttonCompleteGame = dqs('[data-js="button__complete-game"]');
     const $buttonClearGame = dqs('[data-js="button__clear-game"]');
+    const $buttonAddToCart = dqs('[data-js="button__add-to-cart"]');
+
+    const $cartGames = dqs('[data-js="cart-games"]');
+    const $cartGamesTotalPrice = dqs('[data-js="cart-games__total-price"]');
 
     const ajax = new XMLHttpRequest();
 
@@ -21,6 +25,138 @@
     let games = null;
     let selectedGame = null;
     let selectedNumbers = [];
+    let cartGames = [];
+
+    function formatCurrencyToBRL(currency) {
+      return Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(currency);
+    }
+
+    function deleteCartItemById(id) {
+      cartGames = cartGames.filter((cartGame) => cartGame.id !== id);
+      renderCart();
+    }
+
+    function makeButtonDeleteCartItemById(id) {
+      const $buttonDeleteCartItem = doc.createElement("button");
+      $buttonDeleteCartItem.classList.add("cart-game__delete-button");
+      $buttonDeleteCartItem.setAttribute("type", "button");
+      $buttonDeleteCartItem.addEventListener(
+        "click",
+        () => deleteCartItemById(id),
+        false
+      );
+
+      const $trashIcon = doc.createElement("img");
+      $trashIcon.src = "./src/assets/trash-icon.svg";
+      $trashIcon.alt = "Delete game";
+
+      $buttonDeleteCartItem.appendChild($trashIcon);
+
+      return $buttonDeleteCartItem;
+    }
+
+    function makeParagraphWithGameNumbers(cartGame) {
+      const numbers = cartGame.numbers.sort((a, b) => a - b).join();
+
+      const $paragraphWithGameNumbers = doc.createElement("p");
+      $paragraphWithGameNumbers.classList.add("cart-game__game-numbers");
+      $paragraphWithGameNumbers.textContent = numbers;
+
+      return $paragraphWithGameNumbers;
+    }
+
+    function makeSpanGamePrice(cartGame) {
+      const $spanGamePrice = doc.createElement("span");
+      $spanGamePrice.classList.add("cart-game__game-price");
+      $spanGamePrice.textContent = formatCurrencyToBRL(cartGame.price);
+
+      return $spanGamePrice;
+    }
+
+    function makeStrongGameType(cartGame) {
+      const $strongGameType = doc.createElement("strong");
+      $strongGameType.classList.add("cart-game__game-type");
+      $strongGameType.textContent = cartGame.type;
+      $strongGameType.style.color = cartGame.color;
+
+      return $strongGameType;
+    }
+
+    function makeParagraphWithGameInfo(cartGame) {
+      const $paragraphWithGameInfo = doc.createElement("p");
+      $paragraphWithGameInfo.classList.add("cart-game__game-info");
+
+      const $strongGameType = makeStrongGameType(cartGame);
+      const $spanGamePrice = makeSpanGamePrice(cartGame);
+
+      $paragraphWithGameInfo.appendChild($strongGameType);
+      $paragraphWithGameInfo.appendChild($spanGamePrice);
+
+      return $paragraphWithGameInfo;
+    }
+
+    function makeDivCartGameData(cartGame) {
+      const $divCartGameData = doc.createElement("div");
+      $divCartGameData.classList.add("cart-game__game-data");
+      $divCartGameData.style.borderColor = cartGame.color;
+
+      const $paragraphWithGameNumbers = makeParagraphWithGameNumbers(cartGame);
+      const $paragraphWithGameInfo = makeParagraphWithGameInfo(cartGame);
+
+      $divCartGameData.appendChild($paragraphWithGameNumbers);
+      $divCartGameData.appendChild($paragraphWithGameInfo);
+
+      return $divCartGameData;
+    }
+
+    function makeListItemCartGame(cartGame) {
+      const $listItemCartGame = doc.createElement("li");
+      $listItemCartGame.classList.add("cart-game");
+
+      const $buttonDeleteCartItem = makeButtonDeleteCartItemById(cartGame.id);
+      const $divCartGameData = makeDivCartGameData(cartGame);
+
+      $listItemCartGame.appendChild($buttonDeleteCartItem);
+      $listItemCartGame.appendChild($divCartGameData);
+
+      return $listItemCartGame;
+    }
+
+    function updateCartTotalPrice() {
+      const totalPrice = cartGames.reduce((acc, game) => acc + game.price, 0);
+      $cartGamesTotalPrice.textContent = formatCurrencyToBRL(totalPrice);
+    }
+
+    function renderCart() {
+      $cartGames.textContent = "";
+      cartGames.forEach((cartGame) => {
+        const $listItemCartGame = makeListItemCartGame(cartGame);
+        $cartGames.appendChild($listItemCartGame);
+      });
+
+      updateCartTotalPrice();
+    }
+
+    function handleAddGameToCart() {
+      if (selectedNumbers.length !== selectedGame["max-number"]) return;
+
+      const newCartItemId = Math.floor(Math.random() * 10000) + 1;
+      const newCartItem = {
+        id: newCartItemId,
+        type: selectedGame.type,
+        numbers: selectedNumbers,
+        price: selectedGame.price,
+        color: selectedGame.color,
+      };
+
+      cartGames.push(newCartItem);
+
+      renderCart();
+      handleClearGame();
+    }
 
     function handleCompleteGame() {
       while (selectedNumbers.length < selectedGame["max-number"]) {
@@ -149,33 +285,48 @@
 
         if (buttonGameType.value === gameType) {
           selectedGame = getSelectedGameDataByType(gameType);
+
+          buttonGameType.style.background =
+            buttonGameType.getAttribute("data-color");
+          buttonGameType.style.color = "#ffffff";
+
           buttonGameType.classList.add(classGameTypeSelected);
+        } else {
+          buttonGameType.style.background = "transparent";
+          buttonGameType.style.color =
+            buttonGameType.getAttribute("data-color");
         }
       });
 
       initGame();
     }
 
-    function makeGameTypeButton(type) {
+    function makeGameTypeButton(game) {
       const $button = doc.createElement("button");
 
       $button.classList.add("game-type");
       $button.setAttribute("type", "button");
       $button.setAttribute("data-js", "game-type");
-      $button.value = type;
-      $button.textContent = type;
-      $button.addEventListener("click", handleChangeSelectedGameType, false);
+      $button.setAttribute("data-color", game.color);
+      $button.value = game.type;
+      $button.textContent = game.type;
+      $button.style.color = game.color;
+      $button.style.borderColor = game.color;
 
+      $button.addEventListener("click", handleChangeSelectedGameType, false);
       return $button;
     }
 
     function handleRenderGameType(game, index) {
       const $listItemGameType = doc.createElement("li");
-      const $buttonGameType = makeGameTypeButton(game.type);
+      const $buttonGameType = makeGameTypeButton(game);
 
       if (index === 0) {
         selectedGame = getSelectedGameDataByType(game.type);
         $buttonGameType.classList.add(classGameTypeSelected);
+        $buttonGameType.style.background =
+          $buttonGameType.getAttribute("data-color");
+        $buttonGameType.style.color = "#ffffff";
       }
 
       $listItemGameType.appendChild($buttonGameType);
@@ -208,10 +359,12 @@
     function initEvents() {
       $buttonCompleteGame.addEventListener("click", handleCompleteGame, false);
       $buttonClearGame.addEventListener("click", handleClearGame, false);
+      $buttonAddToCart.addEventListener("click", handleAddGameToCart, false);
     }
 
     function init() {
       getGamesData();
+      renderCart();
       initEvents();
     }
 
